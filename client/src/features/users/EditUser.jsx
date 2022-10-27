22c6d4a4f61cd3ab32699726920316b1094ec71c
Siiidali <dadisisaber@gmail.com>
2022-10-27 10:58:19 +0100

    last commit

--- client/src/features/users/EditUser.jsx
+++ client/src/features/users/EditUser.jsx
@@ -1,13 +1,17 @@
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectUserById } from "./usersApiSlice";
 import EditUserForm from "./EditUserForm";
-import React from "react";
+import { useGetUsersQuery } from "./usersApiSlice";
+import useTitle from "../../hooks/useTitle";
 
 const EditUser = () => {
+  useTitle("techNotes: Edit User");
   const { id } = useParams();
 
-  const user = useSelector((state) => selectUserById(state, id));
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      user: data?.entities[id],
+    }),
+  });
 
   const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>;
 
